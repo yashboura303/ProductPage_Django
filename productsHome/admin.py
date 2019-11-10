41106ad63e1fb339d719bfb7eb4d1aa289ac8af4
yashboura303 <yashboura303@gmail.com>
2019-11-11 01:16:08 +0530

    first commit

---
+++ productsHome/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Products, Views
+
+admin.site.register(Products)
+admin.site.register(Views)
---
+++ productsHome/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'productsHome'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<int:product_id>',views.productView, name="productView" )
+]
