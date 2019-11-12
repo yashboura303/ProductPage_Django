f330ee1a78a4c0dc3a592eca3caaa68ec0366d32
yash <yashboura303@gmail.com>
2019-11-12 18:35:58 +0530

    allowed host

--- viewCounter/settings.py
+++ viewCounter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k8o)qi5vy&4_luta0+l8gpw!6&vz6s8wsd%zrldjfkk77(u68c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
